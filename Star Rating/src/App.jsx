ded744fede9993b38b5cfe7128d43e3968c00e87
kapilmogre1998 <kapil.mogre@nobroker.in>
2024-10-07 23:00:34 +0530

    added transition property to have smooth hover effect

--- Star Rating/src/App.jsx
+++ Star Rating/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
 import StarRating from './Common/StarRating/StarRating'
+import './App.css'
 
 function App() {
 
   return (
     <>
+      <div className='title' >Star Rating</div>
       <StarRating count={5} click={(count) => alert({count})} />
     </>
   )
