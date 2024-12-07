099d49a47df196db285508cf2ccf41571a1f319e
kapilmogre1998 <kapil.mogre@nobroker.in>
2024-12-07 23:10:21 +0530

    updated id + 1 with only id

--- Star Rating/src/Common/StarRating/StarRating.jsx
+++ Star Rating/src/Common/StarRating/StarRating.jsx
@@ -6,24 +6,24 @@ const StarRating = ({ count = 5, click }) => {
     const [clickedStarCount, setClickedStarCount] = useState(0);
 
     const handleMouseEnter = (id) => {
-        setHoverRating(id + 1);
+        setHoverRating(id);
     }
 
     const handleClickOnStar = (id) => {
-        setClickedStarCount(id + 1);
+        setClickedStarCount(id);
     }
 
     return (
         <div className='star-rating-container' >
             <div className='star-rating-content' >
                 {
-                    new Array(count).fill(0).map((item, id) => {
-                        const isHover = id + 1 <= hoverRating;
+                    new Array(count).fill(0).map((_, id) => {
+                        const isHover = id <= hoverRating;
 
                         return (
                             <div
                                 key={id}
-                                className={`star ${isHover ? 'gold' : ''} ${id + 1 <= clickedStarCount ? 'gold' : ''}`}
+                                className={`star ${isHover ? 'gold' : ''} ${id <= clickedStarCount ? 'gold' : ''}`}
                                 onMouseOver={() => handleMouseEnter(id)}
                                 onMouseLeave={() => setHoverRating(0)}
                                 onClick={() => handleClickOnStar(id)}
